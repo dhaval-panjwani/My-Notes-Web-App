3afae5ab330aa1625530e4bdd57fd7634ff8a36c
Dhaval Panjwani <panjwanidhaval@gmail.com>
2022-10-16 03:23:26 +0530

    Styling changes

--- src/App.js
+++ src/App.js
@@ -7,50 +7,60 @@ import Header from './components/Header';
 const App = () => {
 
   const initialNotes = [
-		{
-			id: nanoid(),
-			text: 'You are Awesome!',
-			date: '16/10/2022',
-		},
-		{
-			id: nanoid(),
-			text: 'Strategize, Plan, Achieve!',
-			date: '16/10/2022',
-		}
-	];
+    {
+      id: nanoid(),
+      text: 'Articulate the Product Roadmap to the team.',
+      date: '16/10/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'Strategize, Plan, Achieve!',
+      date: '16/10/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'Create the Login journey with UX Team.',
+      date: '16/10/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'Highlight the red flags in the upcoming Stakeholders meeting.',
+      date: '16/10/2022',
+    }
+  ];
 
-	const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('react-notes-app-data')) ?? initialNotes);
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('react-notes-app-data')) ?? initialNotes);
 
-	const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState('');
 
-	const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
-	useEffect(() => {
+  useEffect(() => {
     document.title = "My Notes App"
-	}, []);
+  }, []);
 
-	useEffect(() => {
-		localStorage.setItem(
-			'react-notes-app-data',
-			JSON.stringify(notes)
-		);
-	}, [notes]);
+  useEffect(() => {
+    localStorage.setItem(
+      'react-notes-app-data',
+      JSON.stringify(notes)
+    );
+  }, [notes]);
 
-	const addNote = (text) => {
-		const date = new Date();
-		const newNote = {
-			id: nanoid(),
-			text: text,
-			date: date.toLocaleDateString(),
-		};
-		const newNotes = [...notes, newNote];
-		setNotes(newNotes);
-	};
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
 
-	const deleteNote = (id) => {
-		const newNotes = notes.filter((note) => note.id !== id);
-		setNotes(newNotes);
-	};
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
 
   return (
     <div className={`${darkMode && 'dark-mode'}`}>
@@ -63,6 +73,11 @@ const App = () => {
           handleAddNote={addNote}
           handleDeleteNote={deleteNote} />
       </div>
+
+      <div className='flex justify-center font-semibold' id={`${darkMode && 'by'}`}>Made with
+        <span className='text-[#e25555] ml-2 mr-2'>   &#10084;   </span>
+        by Dhaval Panjwani</div>
+
     </div>
   )
 };
